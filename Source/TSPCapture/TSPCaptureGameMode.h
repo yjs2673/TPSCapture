343e5896ce1b2bd16643a428f6d57478c741533f
yjs2673 <yjs2673@gmail.com>
2026-03-12 00:00:20 +0900

    초기 커밋.

---
+++ Source/TSPCapture/TSPCaptureGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TSPCaptureGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATSPCaptureGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATSPCaptureGameMode();
+};
+
+
+
